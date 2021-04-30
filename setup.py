2740a6b9fa3476372c8dc4e7fd0f401f28586f64
Yuxinya <56122867+Yuxinya@users.noreply.github.com>
2021-04-30 22:34:35 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r",encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mlatticeabc",
+    name="SG_predict",
     version="0.0.1",
     long_description=long_description,
-    url="https://github.com/mlatticeabc",
+    # url="https://github.com/",
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Operating System :: OS Independent",
