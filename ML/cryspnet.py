f411d5f5feffb8268f263efabd56cbeba48cfcb9
Yuxinya <56122867+Yuxinya@users.noreply.github.com>
2021-04-30 19:23:09 +0800

    Add files via upload

---
+++ ML/cryspnet.py
@@ -0,0 +1,108 @@
+from sklearn.preprocessing import MinMaxScaler,StandardScaler
+# import tensorflow as tf 
+import re
+import numpy as np 
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, BatchNormalization
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import r2_score,f1_score,matthews_corrcoef ,make_scorer,recall_score,roc_auc_score,accuracy_score
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras.metrics import top_k_categorical_accuracy
+from keras.optimizers import RMSprop,Adam,SGD
+
+def main_K():
+
+	# X = pd.read_csv('data/one_data.csv').iloc[85161:,5:-6].fillna(0).values.astype(float)
+	# Y = pd.read_csv('data/one_data.csv').iloc[85161:,1].fillna(0).values
+	# X = pd.read_csv('data/one_data.csv').iloc[9440:32464,5:-6].fillna(0).values.astype(float)
+	# Y = pd.read_csv('data/one_data.csv').iloc[9440:32464,1].fillna(0).values
+	# X = pd.read_csv('data/one_data.csv').iloc[32464:54856,5:-6].fillna(0).values.astype(float)
+	# Y = pd.read_csv('data/one_data.csv').iloc[32464:54856,1].fillna(0).values
+	# X = pd.read_csv('data/one_data.csv').iloc[54856:67531,5:-6].fillna(0).values.astype(float) 
+	# Y = pd.read_csv('data/one_data.csv').iloc[54856:67531,1].fillna(0).values
+	# X = pd.read_csv('data/one_data.csv').iloc[67531:76960,5:-6].fillna(0).values.astype(float) 
+	# Y = pd.read_csv('data/one_data.csv').iloc[67531:76960,1].fillna(0).values
+	# X = pd.read_csv('data/one_data.csv').iloc[76960:85161,5:-6].fillna(0).values.astype(float)
+	# Y = pd.read_csv('data/one_data.csv').iloc[76960:85161,1].fillna(0).values
+	#  cubic data are as follows
+	X = pd.read_csv('data/data.csv').iloc[85161:,5:-6].fillna(0).values.astype(float)
+	Y = pd.read_csv('data/data.csv').iloc[85161:,1].fillna(0).values
+
+	# encode class values as integers
+	encoder = LabelEncoder()
+	encoded_Y = encoder.fit_transform(Y)
+	# convert integers to dummy variables (one hot encoding)
+	dummy_y = np_utils.to_categorical(encoded_Y)
+	input_dim,output_dim=len(X[0]),len(dummy_y[0])
+
+	kf = KFold(n_splits=10,shuffle=True)
+	ls = []
+	for train, test in kf.split(X):
+	    # print(len(train),train, len(test),test)
+	    x_train,x_test, y_train, y_test = X[train],X[test],dummy_y[train],dummy_y[test]
+	    res = NN(x_train,x_test,y_train,y_test,input_dim,output_dim)
+	    ls.append(res)
+	return(ls)
+
+
+def NN(x_train,x_test,y_train,y_test,input_dim,output_dim):
+	scaler = StandardScaler()
+	scaler.fit(x_train)
+	x_train = scaler.transform(x_train)
+	x_test = scaler.transform(x_test)
+	model = Sequential()
+	model.add(keras.Input(shape=(input_dim)))
+	model.add(BatchNormalization())
+	model.add(Dense(units=230,activation='relu'))
+	model.add(BatchNormalization())
+	model.add(Dropout(rate=0.2))
+	model.add(Dense(units=178,activation='relu'))
+	model.add(BatchNormalization())
+	model.add(Dropout(rate=0.2))
+	model.add(Dense(units=280,activation='relu'))
+	model.add(BatchNormalization())
+	model.add(Dropout(rate=0.2))
+	model.add(Dense(units=244,activation='relu'))
+	model.add(BatchNormalization())
+	model.add(Dropout(rate=0.2))
+	model.add(Dense(units=181,activation='relu'))
+	model.add(BatchNormalization())
+	model.add(Dropout(rate=0.2))
+	model.add(Dense(units=output_dim,activation='softmax'))
+	# print(model.summary())
+
+	model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+	early_stopping = EarlyStopping(monitor='loss',patience=30,verbose=1)
+	model.fit(x=x_train,y=y_train, epochs=2000, batch_size=256, verbose=2,callbacks=[early_stopping,])
+	acc = model.evaluate(x_test, y_test)
+	y_pred = model.predict(x_test)
+	y_pred = np.argmax(y_pred,axis=1)
+	y_test = np.argmax(y_test,axis=1)
+
+	a = accuracy_score(y_test,y_pred)
+	mcc = matthews_corrcoef(y_test,y_pred)
+	f1 = f1_score(y_test, y_pred, average='weighted')
+	res = [a,mcc,f1]
+	print(res)
+	return(res)
+
+
+
+
+result = main_K()
+print('accuracy   :','%.3f'%np.mean(result,axis=0)[0],'+-','%.3f'%np.std(result,axis=0)[0])
+print('test_MCC   :','%.3f'%np.mean(result,axis=0)[1],'+-','%.3f'%np.std(result,axis=0)[1])
+print('f1_weighted:','%.3f'%np.mean(result,axis=0)[2],'+-','%.3f'%np.std(result,axis=0)[2])
---
+++ ML/yong.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd 
+
+from sklearn.metrics import f1_score, matthews_corrcoef ,make_scorer,recall_score,roc_auc_score
+from sklearn.metrics import top_k_accuracy_score
+
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+# x = pd.read_csv('data/data.csv').iloc[:9440,5:137].fillna(0).values 
+# y = pd.read_csv('data/data.csv').iloc[:9440,1].fillna(0).values 
+# x = pd.read_csv('data/data.csv').iloc[9440:32464,5:137].fillna(0).values 
+# y = pd.read_csv('data/data.csv').iloc[9440:32464,1].fillna(0).values
+# x = pd.read_csv('data/data.csv').iloc[32464:54856,5:137].fillna(0).values 
+# y = pd.read_csv('data/data.csv').iloc[32464:54856,1].fillna(0).values
+# x = pd.read_csv('data/data.csv').iloc[54856:67531,5:137].fillna(0).values 
+# y = pd.read_csv('data/data.csv').iloc[54856:67531,1].fillna(0).values
+# x = pd.read_csv('data/data.csv').iloc[67531:76960,5:137].fillna(0).values 
+# y = pd.read_csv('data/data.csv').iloc[67531:76960,1].fillna(0).values
+# x = pd.read_csv('data/data.csv').iloc[76960:85161,5:137].fillna(0).values 
+# y = pd.read_csv('data/data.csv').iloc[76960:85161,1].fillna(0).values
+
+#  cubic data are as follows
+x = pd.read_csv('data/data.csv').iloc[85161:,5:137].fillna(0).values 
+y = pd.read_csv('data/data.csv').iloc[85161:,1].fillna(0).values
+
+p = np.random.permutation(range(len(x)))
+x,y = x[p],y[p] #Disrupt the order of data
+
+forest = RandomForestClassifier(criterion='entropy',n_estimators=100,max_features=80,max_depth=None,min_samples_leaf=1,min_samples_split=2,n_jobs=-1) 
+
+scoring = {
+'accuracy' : 'accuracy',
+# 'f1_micro' : 'f1_micro',
+# 'f1_macro' : 'f1_macro',
+'f1_weighted' : 'f1_weighted',
+'MCC':make_scorer(matthews_corrcoef) ,
+}
+scores = cross_validate(forest, x, y, scoring=scoring, cv=10, )
+
+accuracy = scores['test_accuracy']
+mcc = scores['test_MCC']
+f1_weighted = scores['test_f1_weighted']
+
+print('accuracy   :','%.3f'%np.mean(accuracy),'+-','%.3f'%np.std(accuracy,ddof=1))
+print('test_MCC   :','%.3f'%np.mean(mcc),'+-','%.3f'%np.std(mcc,ddof=1))
+print('f1_weighted:','%.3f'%np.mean(f1_weighted),'+-','%.3f'%np.std(f1_weighted,ddof=1))
